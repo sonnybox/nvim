a2bcaa2db008edd30a5397433cdc00c34240bb3b
Sonny Box <sonny@sonnybox.com>
2024-05-10 19:17:49 -0700

    minor alacritty autocmd fix

--- lua/general/autocommands.lua
+++ lua/general/autocommands.lua
@@ -78,7 +78,7 @@ vim.api.nvim_create_autocmd('VimEnter', {
 	group = alacrittyAutoGroup,
 	callback = function()
 		vim.fn.system(
-			"alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID options 'window.padding.x=0' 'window.padding.y=0'"
+			"alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID 'window.padding.x=0' 'window.padding.y=0'"
 		)
 	end,
 })
